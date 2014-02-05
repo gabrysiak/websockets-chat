0b2d16c89d042c3deda6ea17878998151a174dac
Tom Gabrysiak <tomgabrysiak@Toms-MacBook-Pro.local>
2014-02-05 17:47:54 -0500

    added whisper

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-    nicknames = [];
+    mongoose = require('mongoose'),
+    users = {};
 
 server.listen(3000);
+
+mongoose.connect('mongodb://localhost/chat', function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Connected to mongodb');
+    }
+});
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
@@ -12,18 +22,48 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', function(socket) {
     socket.on('new user', function(data, callback) {
-        if (nicknames.indexOf(data) != -1) {
+        if (data in users) {
             callback(false);
         } else {
             callback(true);
             socket.nickname = data;
-            nicknames.push(socket.nickname);
-            io.sockets.emit('usernames', nicknames);
+            users[socket.nickname] = socket;
+            updateNicknames();
         }
     });
 
+    function updateNicknames() {
+        io.sockets.emit('usernames', Object.keys(users));
+    }
 
-    socket.on('send message', function(data) {
-        io.sockets.emit('new message', data);
+    socket.on('send message', function(data, callback) {
+        var msg = data.trim();
+        if(msg.substr(0,3) === '/w ') {
+            msg = msg.substr(3);
+            var ind = msg.indexOf(' ');
+            if(ind !== -1) {
+                var name = msg.substring(0, ind);
+                var msg = msg.substring(ind + 1);
+                if(name in users) {
+                    users[name].emit('whisper', {msg: msg, nick: socket.nickname, type: 'whisper'});
+                    console.log('Whisper');
+                } else {
+                    io.sockets.emit('error', {msg: 'Error: Enter a valid user.', type: 'error'});
+                    // callback('Error: Enter a valid user');
+                }
+                
+            } else {
+                // callback('Error: Please enter a message for your whisper.');
+                io.sockets.emit('error', {msg: 'Please enter a message for your whisper.', type: 'error'});
+            }
+        } else {
+            io.sockets.emit('new message', {msg: msg, nick: socket.nickname, type: 'new-message'});
+        }
+    });
+
+    socket.on('disconnect', function(data) {
+        if(!socket.nickname) return;
+        delete users[socket.nickname];
+        updateNicknames();
     });
 });
